dd9407eaaf4bf62b9d72333aace2311364a83f8c
Julia Kent <jkent@ucar.edu>
2021-05-14 13:02:30 -0600

    add javascript for modal

--- content/_ext/yaml_gallery_generator.py
+++ content/_ext/yaml_gallery_generator.py
@@ -132,41 +132,27 @@ def build_from_items(items, filename, display_name, menu_html):
 {item['description']}
 ```
 
-<!-- Trigger/Open The Modal -->
 <button id="myBtn-{item["title"]}">Open Modal</button>
-
-<!-- The Modal -->
-<div id="myModal-{item["title"]}" class="modal">
-
-  <!-- Modal content -->
+<div id="myModal-{item["title"]}" class="galmodal">
   <div class="modal-content">
-    <span class="modal-close">&times;</span>
+    <span class="galmodal-close">&times;</span>
     <p>Some text in the Modal..</p>
   </div>
-
 </div>
 
 <script>
-// Get the modal
 var modal = document.getElementById("myModal-{item["title"]}");
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn-{item["title"]}");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("modal-close")[0];
 
-// When the user clicks the button, open the modal 
 btn.onclick = function() {{
   modal.style.display = "block";
 }}
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {{
   modal.style.display = "none";
 }}
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {{
   if (event.target == modal) {{
     modal.style.display = "none";
