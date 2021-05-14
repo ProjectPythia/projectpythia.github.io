4fd807a213fd23c934aa8fe4b9990ad5b3577519
Julia Kent <jkent@ucar.edu>
2021-05-14 11:41:39 -0600

    first pass at modals

--- content/_ext/yaml_gallery_generator.py
+++ content/_ext/yaml_gallery_generator.py
@@ -131,6 +131,48 @@ def build_from_items(items, filename, display_name, menu_html):
 {item['description']}
 ```
 
+<!-- Trigger/Open The Modal -->
+<button id="myBtn-{item["title"]}">Open Modal</button>
+
+<!-- The Modal -->
+<div id="myModal-{item["title"]}" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="modal-close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal-{item["title"]}");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn-{item["title"]}");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal-close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {{
+  modal.style.display = "block";
+}}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {{
+  modal.style.display = "none";
+}}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {{
+  if (event.target == modal) {{
+    modal.style.display = "none";
+  }}
+}}
+</script>
+
 ```{{link-button}} {item["url"]}
 :type: url
 :text: Visit Website
@@ -138,8 +180,8 @@ def build_from_items(items, filename, display_name, menu_html):
 ```
 
 {tags}
-"""
-        )
+""")
+
     panels_body = '\n'.join(panels_body)
 
     panels = f"""
--- content/conf.py
+++ content/conf.py
@@ -42,6 +42,7 @@ exclude_patterns = [
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["custom.css"]
 html_logo = '_static/images/ProjectPythia_Logo_Final-01-Blue.svg'
 html_title = ''
 # html_logo = '_static/images/ProjectPythia_Logo_Final-01-Blue-NoText.svg'
