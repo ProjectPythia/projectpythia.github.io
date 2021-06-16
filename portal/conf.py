b5e874a3263b58e1512b4a49341fb57448ff7bdf
Kevin Paul <kpaul@ucar.edu>
2021-06-16 10:08:21 -0600

    Update Sphinx config for new theme

--- portal/conf.py
+++ portal/conf.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 import datetime
-import os
-import pathlib
 import shutil
+import os
 import sys
-from textwrap import dedent
+sys.path.insert(0, os.path.abspath('_extensions'))
 
-import yaml
 
-sys.path.insert(0, os.path.abspath(os.path.join('_extensions')))
-project = 'Pythia Portal'
-copyright = f'2020-{datetime.datetime.now().year}'
+# -- Project information -----------------------------------------------------
+
+project = 'Project Pythia'
 author = 'Project Pythia Developers & Contributors'
-html_last_updated_fmt = '%b %d, %Y'
+copyright = f'2020-{datetime.datetime.now().year}, {author}'
 
-extensions = ['myst_nb', 'sphinx_panels', 'yaml_gallery_generator']
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'myst_nb',
+    'sphinx_panels',
+    'pythia',
+    'yaml_gallery_generator',
+]
+
+# Define what extensions will parse which kind of source file
+source_suffix = {
+    '.ipynb': 'myst-nb',
+    '.myst': 'myst-nb',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -24,19 +50,23 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [
-    '_build',
-    'Thumbs.db',
-    '.DS_Store',
-    '*import_posts*',
-    '**/pandoc_ipynb/inputs/*',
-    '**.ipynb_checkpoints',
-]
+exclude_patterns = []
+
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
+#
+html_theme = 'sphinx_pythia_theme'
+html_theme_path = ['..']
+html_last_updated_fmt = '%d %B %Y'
+
+# Favicon
+html_favicon = '_static/images/icons/favicon.ico'
+
+# Permalinks Icon
+html_permalinks_icon = '<i class="bi bi-link"></i>'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -44,29 +74,24 @@ exclude_patterns = [
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 html_js_files = ['custom.js']
-html_logo = '_static/images/ProjectPythia_Logo_Final-01-Blue.svg'
-html_title = ''
-# html_logo = '_static/images/ProjectPythia_Logo_Final-01-Blue-NoText.svg'
-# html_title = 'Project Pythia'
-html_favicon = '_static/images/ProjectPythia_Logo_Final-01-Blue.ico'
 
-html_theme = 'sphinx_book_theme'
+# HTML Theme-specific Options
 html_theme_options = {
-    # 'home_page_in_toc': True,
-    'path_to_docs': './',
-    'search_bar_text': 'Search this site...',
-    'repository_url': 'https://github.com/NCAR/pythia-portal',
-    # "repository_branch": "gh-pages",  # For testing
-    'launch_buttons': {
-        'binderhub_url': 'https://mybinder.org',
-        # "jupyterhub_url": "https://datahub.berkeley.edu",  # For testing
-        'colab_url': 'https://colab.research.google.com/',
-        'notebook_interface': 'jupyterlab',
-        'thebe': False,
+    'onepagers': [
+        'index',
+    ],
+    'logos_bar': {
+        'NCAR': '_static/images/logos/NCAR-contemp-logo-blue.svg',
+        'Unidata': '_static/images/logos/Unidata_logo_horizontal_1200x300.svg',
+        'UAlbany': '_static/images/logos/UAlbany-A2-logo-purple-gold.svg',
     },
-    'use_edit_page_button': False,
-    'use_issues_button': False,
-    'use_repository_button': False,
+    'sponsor_text': 'This material is based upon work supported by the National Science Foundation under Grant Nos. 2026863 and 2026899. Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation.',
+    'sponsor_logo': '_static/images/logos/footer-logo-nsf.png',
+}
+
+# Disable sidebars on all Portal Pages
+html_sidebars = {
+    '**': [],
 }
 
 # Panels config
@@ -77,8 +102,8 @@ myst_enable_extensions = ['amsmath', 'colon_fence', 'deflist', 'html_image']
 myst_url_schemes = ('http', 'https', 'mailto')
 jupyter_execute_notebooks = 'off'
 
-# CUSTOM SCRIPTS =============================================================
+# CUSTOM SCRIPTS ==============================================================
 
-# Copy root files into content pages -----------------------------------------
+# Copy root files into content pages ------------------------------------------
 
 shutil.copyfile('../CODEOFCONDUCT.md', 'code_of_conduct.md')
