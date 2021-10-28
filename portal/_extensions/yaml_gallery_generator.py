d1ad047fba60040609dfb0b707f0fdac4519c474
Julia Kent <jkent@ucar.edu>
2021-10-28 11:06:39 -0600

    rm old filtering

--- portal/_extensions/yaml_gallery_generator.py
+++ portal/_extensions/yaml_gallery_generator.py
@@ -54,15 +54,13 @@ def _generate_tag_menu(all_items, tag_key):
 """
 
 
-def _generate_menu(all_items, flt=None):
+def _generate_menu(all_items):
 
     key_list = _generate_sorted_tag_keys(all_items)
 
     menu_html = '<div class="d-sm-flex mt-3 mb-4">\n'
     menu_html += '<div class="d-flex gallery-menu">\n'
     menu_html += '<div><a role="button" class="btn btn-primary btn-sm mx-1" href="https://github.com/ProjectPythia/projectpythia.github.io/issues/new?assignees=&labels=external-links-gallery-submission&template=update-external-links-gallery.md&title=">Submit a new resource</a></div>\n'
-    if flt:
-        menu_html += '<div><a role="button" class="btn btn-link btn-sm text-decoration-none" href="/gallery.html">Return to Full Gallery</a></div>\n'
     menu_html += '</div>\n'
     menu_html += '<div class="ms-auto d-flex">\n'
     for tag_key in key_list:
@@ -87,7 +85,7 @@ def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html=
         tags = [f'<a href="/gallery/{tag}.html" class="badge bg-primary link-light">{tag}</a>' for tag in tag_list_f]
         tags = '\n'.join(tags)
 
-        tag_str = ' '.join(tag_list_f)
+        tag_class_str = ' '.join(tag_list_f)
 
         author_strs = set()
         institution_strs = set()
@@ -141,7 +139,7 @@ def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html=
             f"""\
 ---
 
-<div class="d-flex gallery-card {tag_str}">
+<div class="d-flex gallery-card {tag_class_str}">
 <img src="{thumbnail}" class="gallery-thumbnail" />
 <div class="container">
 <a href="{item["url"]}" class="text-decoration-none"><h4 class="display-4 p-0">{item["title"]}</h4></a>
@@ -197,13 +195,6 @@ def main(app):
     title = 'Pythia Resource Gallery'
     menu_html = _generate_menu(all_items)
     build_from_items(all_items, 'gallery', title=title, menu_html=menu_html)
-
-    menu_html_flt = _generate_menu(all_items, flt=True)
-    tag_set = _generate_tag_set(all_items)
-
-    for tag in tag_set:
-        items = [item for item in all_items if _tag_in_item(item, tag)]
-        build_from_items(items, f'gallery/{tag.replace(" ", "-")}', title=title, subtitle=tag, menu_html=menu_html_flt)
 
 
 def setup(app):
