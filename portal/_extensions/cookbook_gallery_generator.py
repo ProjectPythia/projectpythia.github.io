dfc5d27f8cd2974f52781bb9f19ab163d40ed4e0
Julia Kent <jkent@ucar.edu>
2022-05-17 10:52:32 -0600

    update cookbook gallery generator

--- portal/_extensions/cookbook_gallery_generator.py
+++ portal/_extensions/cookbook_gallery_generator.py
@@ -6,66 +6,6 @@ import yaml
 from truncatehtml import truncate
 
 
-def _generate_sorted_tag_keys(all_items):
-
-    key_set = set(itertools.chain(*[item['tags'].keys() for item in all_items]))
-    return sorted(key_set)
-
-
-def _generate_tag_set(all_items, tag_key=None):
-
-    tag_set = set()
-    for item in all_items:
-        for k, e in item['tags'].items():
-            if tag_key and k != tag_key:
-                continue
-            for t in e:
-                tag_set.add(t)
-
-    return tag_set
-
-
-def _generate_tag_menu(all_items, tag_key):
-
-    tag_set = _generate_tag_set(all_items, tag_key)
-    tag_list = sorted(tag_set)
-
-    options = ''.join(
-        f'<li><label class="dropdown-item checkbox {tag_key}"><input type="checkbox" rel={tag.replace(" ", "-")} onchange="change();">&nbsp;{tag.capitalize()}</label></li>'
-        for tag in tag_list
-    )
-
-    return f"""
-<div class="dropdown">
-
-<button class="btn btn-sm btn-outline-primary mx-1 dropdown-toggle" type="button" id="{tag_key}Dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-{tag_key.title()}
-</button>
-<ul class="dropdown-menu" aria-labelledby="{tag_key}Dropdown">
-{options}
-</ul>
-</div>
-"""
-
-
-def _generate_menu(all_items):
-
-    key_list = _generate_sorted_tag_keys(all_items)
-
-    menu_html = '<div class="d-sm-flex mt-3 mb-4">\n'
-    menu_html += '<div class="d-flex gallery-menu">\n'
-    menu_html += '<div><a role="button" class="btn btn-primary btn-sm mx-1" href="https://github.com/ProjectPythia/projectpythia.github.io/issues/new?assignees=&labels=resource-gallery-submission&template=update-resource-gallery.md&title=">Submit a new resource</a></div>\n'
-    menu_html += '</div>\n'
-    menu_html += '<div class="ml-auto d-flex">\n'
-    menu_html += '<div><button class="btn btn-link btn-sm mx-1" onclick="clearCbs()">Clear all filters</button></div>\n'
-    for tag_key in key_list:
-        menu_html += _generate_tag_menu(all_items, tag_key) + '\n'
-    menu_html += '</div>\n'
-    menu_html += '</div>\n'
-    menu_html += '<script>$(document).on("click",function(){$(".collapse").collapse("hide");}); </script>\n'
-    return menu_html
-
-
 def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html='', max_descr_len=300):
 
     # Build the gallery file
@@ -74,13 +14,6 @@ def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html=
         if not item.get('thumbnail'):
             item['thumbnail'] = '/_static/images/ebp-logo.png'
         thumbnail = item['thumbnail']
-        tag_list = sorted((itertools.chain(*item['tags'].values())))
-        tag_list_f = [tag.replace(' ', '-') for tag in tag_list]
-
-        tags = [f'<span class="badge bg-primary">{tag}</span>' for tag in tag_list_f]
-        tags = '\n'.join(tags)
-
-        tag_class_str = ' '.join(tag_list_f)
 
         author_strs = set()
         institution_strs = set()
@@ -132,7 +65,7 @@ def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html=
         panels_body.append(
             f"""\
 ---
-:column: + tagged-card {tag_class_str}
+:column:
 
 <div class="d-flex gallery-card">
 <img src="{thumbnail}" class="gallery-thumbnail" />
@@ -143,10 +76,6 @@ def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html=
 </div>
 </div>
 {modal_str}
-
-+++
-
-{tags}
 
 """
         )
@@ -184,12 +113,12 @@ def build_from_items(items, filename, title='Gallery', subtitle=None, menu_html=
 
 def main(app):
 
-    with open('resource_gallery.yaml') as fid:
+    with open('cookbook_gallery.yaml') as fid:
         all_items = yaml.safe_load(fid)
 
-    title = 'Pythia Resource Gallery'
+    title = 'Pythia Cookbooks Gallery'
     menu_html = _generate_menu(all_items)
-    build_from_items(all_items, 'resource-gallery', title=title, menu_html=menu_html)
+    build_from_items(all_items, 'cookbook-gallery', title=title, menu_html=menu_html)
 
 
 def setup(app):
