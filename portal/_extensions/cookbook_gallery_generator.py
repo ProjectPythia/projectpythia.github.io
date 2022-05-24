59348e314b15317a156530ea8600dd6680c66a6f
Julia Kent <jkent@ucar.edu>
2022-05-24 17:21:05 -0600

    fix syntax error

--- portal/_extensions/cookbook_gallery_generator.py
+++ portal/_extensions/cookbook_gallery_generator.py
@@ -10,7 +10,7 @@ def main(app):
     title = 'Pythia Cookbooks Gallery'
     subtitle = 'Pythia Cookbooks provide example workflows on more advanced and domain-specific problems developed by the Pythia community. Cookbooks build on top of skills you learn in Pythia Foundations.'
     menu_html = generate_menu(all_items)
-    build_from_items(all_items, 'cookbook-gallery', title=title, subtitle=subtitle, , menu_html=menu_html)
+    build_from_items(all_items, 'cookbook-gallery', title=title, subtitle=subtitle, menu_html=menu_html)
 
 
 def setup(app):
