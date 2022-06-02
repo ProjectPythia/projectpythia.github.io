3e4f14a851502133923ba1f0c0a3a4c98815f624
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-06-02 12:18:43 -0600

    rm pythia from gallery titles (#257)

--- portal/_extensions/cookbook_gallery_generator.py
+++ portal/_extensions/cookbook_gallery_generator.py
@@ -7,7 +7,7 @@ def main(app):
     with open('cookbook_gallery.yaml') as fid:
         all_items = yaml.safe_load(fid)
 
-    title = 'Pythia Cookbooks Gallery'
+    title = 'Cookbooks Gallery'
     subtext = 'Pythia Cookbooks provide example workflows on more advanced and domain-specific problems developed by the Pythia community. Cookbooks build on top of skills you learn in Pythia Foundations.'
     menu_html = generate_menu(all_items)
     build_from_items(all_items, 'cookbook-gallery', title=title, subtext=subtext, menu_html=menu_html)
