5ac1362e86978df2b2e58944743b97a14b94ae76
Julia Kent <46687291+jukent@users.noreply.github.com>
2024-03-11 16:58:39 -0600

    Metrics API test (#408)
    * Create automate-metrics.yaml
    
    * outline for automate-metrics.yaml
    
    * Create get-metrics.py
    
    * Update automate-metrics.yaml
    
    * Update automate-metrics.yaml
    
    * Update footer-menu.html with metrics
    
    * Create metrics.md
    
    * add json_extract to extensions
    
    * Update get-metrics.py to also write file
    
    * Update get-metrics.py with returned condition
    
    * Update automate-metrics.yaml
    
    * pre-commit
    
    * use os to access passed in keys
    
    * fix double quoted strings
    
    * add jsonextract to environment.yml
    
    * write metrics.md in a python file
    
    * rm from config
    
    * use env files
    
    * update comment
    
    * state with a table
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rm table because it won't format and add to toc tree
    
    * add dispatch
    
    * starting metrics file, and - vs _
    
    * pre-commit
    
    * fix write-metrics
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ .github/workflows/write-metrics-md.py
@@ -0,0 +1,33 @@
+import json
+
+
+def process_user_data(user_data_file, markdown_file):
+    """
+    Reads user data from a JSON file and writes it to a markdown file.
+
+    Args:
+        user_data_file: Path to the JSON file containing user data.
+        markdown_file: Path to the output markdown file.
+    """
+
+    with open(user_data_file, 'r') as f:
+        user_data = json.load(f)
+
+    # table_header = '| Portal | Foundations | Cookbooks |\n'
+    # table_row = f"| {' | '.join([str(user_data[key]) for key in user_data])} |\n"
+    # table = table_header + table_row
+
+    # Write processed data to markdown file
+    with open(markdown_file, 'w') as f:
+        f.write('# Metrics \n\n')
+        f.write('Total Users:\n\n')
+        for key in user_data:
+            f.write(f'{key}: {user_data[key]}\n')
+    f.close()
+
+
+if __name__ == '__main__':
+    user_data_file = 'user_metrics.json'
+    markdown_file = '../../portal/metrics.md'
+    process_user_data(user_data_file, markdown_file)
+    print('User data report generated: ', markdown_file)
