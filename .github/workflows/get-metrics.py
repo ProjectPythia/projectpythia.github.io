b8a47fcef37fac91303a36ce5a680669a801fb05
Julia Kent <46687291+jukent@users.noreply.github.com>
2024-03-15 13:33:38 -0600

    Update get-metrics.py

--- .github/workflows/get-metrics.py
+++ .github/workflows/get-metrics.py
@@ -14,8 +14,14 @@ if 'GOOGLE_ANALYTICS_CREDENTIALS' not in os.environ:
     exit(1)
 else:
     encoded_credentials = os.environ.get('GOOGLE_ANALYTICS_CREDENTIALS')
-    print(encoded_credentials)
+    print('encoded_credentials')
+    print(type(encoded_credentials)
 
+decoded_credentials = base64.b64decode(encoded_credentials).decode('utf-8')
+print(type(decoded_credentials)
+credentials_dict = json.loads(decoded_credentials)
+print(credentials_dict)
+client = BetaAnalyticsDataClient.from_service_account_info(credentials_dict)
 
 
 def _run_total_users_report(property_id):
