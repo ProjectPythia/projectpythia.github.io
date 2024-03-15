7dc9faadb751b85926e1ee75785c8f5a8ec51b9c
Julia Kent <46687291+jukent@users.noreply.github.com>
2024-03-15 15:21:41 -0600

    Update get-metrics.py

--- .github/workflows/get-metrics.py
+++ .github/workflows/get-metrics.py
@@ -9,29 +9,27 @@ from google.analytics.data_v1beta.types import DateRange, Metric, RunReportReque
 PORTAL_ID = os.environ['PORTAL_ID']
 FOUNDATIONS_ID = os.environ['FOUNDATIONS_ID']
 COOKBOOKS_ID = os.environ['COOKBOOKS_ID']
-PRIVATE_KEY_ID = os.environ.get('PRIVATE_KEY_ID')
-PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
-print(len(PRIVATE_KEY_ID))
-print(type(PRIVATE_KEY_ID))
-print(len(PRIVATE_KEY))
-print(type(PRIVATE_KEY))
-PRIVATE_KEY.replace('$','\n')
-print(len(PRIVATE_KEY))
 
-      
-credentials_dict = {
-  "type": "service_account",
-  "project_id": "cisl-vast-pythia",
-  "private_key_id": str(PRIVATE_KEY_ID),
-  "private_key": str(PRIVATE_KEY),
-  "client_email": "pythia-metrics-api@cisl-vast-pythia.iam.gserviceaccount.com",
-  "client_id": "113402578114110723940",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pythia-metrics-api%40cisl-vast-pythia.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
+#PRIVATE_KEY_ID = os.environ.get('PRIVATE_KEY_ID')
+#PRIVATE_KEY = os.environ.get('PRIVATE_KEY').replace('$','\n')
+#credentials_dict = {
+#  "type": "service_account",
+#  "project_id": "cisl-vast-pythia",
+#  "private_key_id": PRIVATE_KEY_ID,
+#  "private_key": PRIVATE_KEY,
+#  "client_email": "pythia-metrics-api@cisl-vast-pythia.iam.gserviceaccount.com",
+#  "client_id": "113402578114110723940",
+#  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#  "token_uri": "https://oauth2.googleapis.com/token",
+#  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pythia-metrics-api%40cisl-vast-pythia.iam.gserviceaccount.com",
+#  "universe_domain": "googleapis.com"
+#}
+
+ENCODED_CREDENTIALS = os.environ.get('ENCODED_CREDENTIALS')
+decoded_credentials = base64.b64decode(ENCODED_CREDENTIALS).decode('utf-8')
+credentials_dict = json.loads(decoded_credentials)
+
 client = BetaAnalyticsDataClient.from_service_account_info(credentials_dict)
 
 
