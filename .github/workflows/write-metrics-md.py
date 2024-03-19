848d6968fa42e6a64b339bf0b7f9bb64da6b69c9
Julia Kent <46687291+jukent@users.noreply.github.com>
2024-03-19 16:07:59 -0600

    remove map for now

--- .github/workflows/write-metrics-md.py
+++ .github/workflows/write-metrics-md.py
@@ -1,7 +1,7 @@
 import json
 
 
-def process_user_data(json_file, top_pages, this_year, map, md_file):
+def process_user_data(json_file, top_pages, this_year, md_file):
     with open(json_file, 'r') as f:
         user_data = json.load(f)
 
@@ -23,7 +23,6 @@ def process_user_data(json_file, top_pages, this_year, map, md_file):
 
         f.write(f'![Top Pages]({top_pages})\n\n')
         f.write(f'![Users this Year]({this_year})\n\n')
-        f.write(f'![Users by Country]({map})\n\n')
 
     f.close()
 
@@ -32,6 +31,5 @@ if __name__ == '__main__':
     json_file = 'portal/metrics/user_metrics.json'
     top_pages = 'metrics/toppages.png'
     this_year = 'metrics/thisyear.png'
-    map = 'metrics/bycountry.png'
     md_file = 'portal/metrics.md'
-    process_user_data(json_file, top_pages, this_year, map, md_file)
+    process_user_data(json_file, top_pages, this_year, md_file)
